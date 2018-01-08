04241b28da0e4a19e869eb0b4f3279397343054b
sktperiasamy <31505529+sktperiasamy@users.noreply.github.com>
2018-01-08 16:24:43 +0530

    Create naveen1.c

---
+++ naveen1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("\n enter the no");
+scanf("%d",&n);
+if(n>0)
+{
+printf("\n the no is positive");
+}
+else if(n<0)
+{
+printf("\n tht no is negative");
+}
+else
+{
+printf(" the no is zero");
+}
+return 0;
+}
