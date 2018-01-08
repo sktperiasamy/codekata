c433a86e228daaf2dd4cce8d4de5fb1df116fc7a
sktperiasamy <31505529+sktperiasamy@users.noreply.github.com>
2018-01-08 16:29:38 +0530

    Create naveen2.c

---
+++ naveen2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("\n enter the no");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("\n the no is even");
+}
+else
+{
+printf(" the no is odd");
+}
+return 0;
+}
