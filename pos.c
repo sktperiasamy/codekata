0020e83d1da6ef9d51fb76ee73c4a764d44bd011
sktperiasamy <31505529+sktperiasamy@users.noreply.github.com>
2018-01-22 21:28:20 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("Enter the number",a);
+scanf("%d",&a);
+if(a>0)
+{
+printf("Given number is positive");
+}
+else if(a=0)
+{
+printf("Given number is zero");
+}
+else(a<0)
+{
+printf("Given number is negative");
+}
+getch();
+}
